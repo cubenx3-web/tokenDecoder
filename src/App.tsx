53a72d6c6dd2a86b374f45fefd28ed97bec86690
cubenx3-web <cubenx3@gmail.com>
2026-05-28 00:20:29 +0300

    update ui

--- src/App.tsx
+++ src/App.tsx
@@ -27,17 +27,17 @@ function App() {
 
   return (
     <>
-      <div className="bg-black w-dvw h-dvh relative text-gray-200 shadow-2xl flex flex-col shadow-orange-50 items-center justify-center">
+      <div className="bg-black w-dvw h-dvh relative text-gray-200 shadow-2xl flex flex-col shadow-orange-50 items-center justify-center gap-10">
         <form 
         onSubmit={submitHandler}
-        className={` bg-gray-900/50 backdrop-blur-2xl min-w-100 p-3 gap-5 min-h-50 border flex flex-col rounded-lg -translate-y-1/2`}
+        className={` bg-gray-900/50 backdrop-blur-2xl min-w-100 p-3 gap-5 min-h-50 border flex flex-col rounded-lg `}
         >
           <div className={`text-2xl font-bold text-center w-full`}>Token Decoder</div>
           <textarea className="border rounded-lg p-2 min-h-30" placeholder="Enter Your Jwt Token" value={token} onChange={(e)=>(setToken(e.target.value))} required></textarea>
           
           <button type="submit" className={`bg-blue-600 backdrop-blur-lg hover:bg-blue-400/40 active:bg-blue-800 p-1 rounded-lg`}>Decode</button>
         </form>
-        <div className="border p-5 text-2xl rounded-lg ring-3 ring-blue-400/60">
+        <div className="border p-5 text-sm rounded-lg ring-3 ring-blue-400/60">
           {
             decode && Object.entries(decode).map(([key, value]) => (
               <div className="" key={key}>{key} : {value.toString()}</div>
